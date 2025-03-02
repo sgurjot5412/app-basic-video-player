b306cd24da54f771e3d73dc0e7867f40d9c28d60
Gurjot <gurjot.mehta@magicpin.in>
2025-03-02 14:52:54 +0530

    changed state from var to val

--- app/src/main/java/com/pubscale/basicvideoplayer/domain/model/VideoState.kt
+++ app/src/main/java/com/pubscale/basicvideoplayer/domain/model/VideoState.kt
@@ -2,6 +2,6 @@ package com.pubscale.basicvideoplayer.domain.model
 
 sealed class VideoState {
     object Loading : VideoState()
-    data class Success(var url: String) : VideoState()
+    data class Success(val url: String) : VideoState()
     data class Error(val message: String) : VideoState()
 }
